98ddc83a931327fe39018ed47949e1c9d83dbfe9
Sriram Sundarraj <ssundarraj@gmail.com>
2026-07-30 14:42:55 -0700

    nanogpt: gpt.py

---
+++ nanogpt/gpt.py
@@ -0,0 +1,260 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+torch.manual_seed(1337)
+
+# hyperparameters
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = (
+    "cuda" if torch.cuda.is_available()
+    # else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"using device: {device}")
+eval_iters = 200
+# ------------
+
+'''
+going to train a char level model on shakespeare's works
+reference code for video: https://github.com/karpathy/ng-video-lecture
+finished code: https://github.com/karpathy/build-nanogpt
+'''
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+'''
+we train the transformer on chunks of the dataset, not the entire dataset (block_size/context_len)
+the transformer makes predictions at all the next chars for given block size simultaneously
+
+x has batch_size rows
+for each row, there are block_size examples we can use to train the model
+how this works is that, for input x[b][0] -> y[b][0]
+for input x[b][0,1] -> y[b][1]
+for input x[b][0,1,2] -> y[b][2]
+... etc
+
+we do this because we can train on much more data and also we can run inference on a single token
+'''
+def dbg_input_output(): # to understand the shape of input/outputs
+    xb, yb = get_batch('train')
+    print(xb.shape, yb.shape)
+    print(xb)
+    print(yb)
+    print('----')
+    print('----')
+
+    for b in range(batch_size):
+        print('----batch:', b)
+        print(xb[b])
+        print(yb[b])
+        for t in range(block_size):
+            context = xb[b, :t+1]
+            target = yb[b, t]
+            print (f"input {context} target {target}")
+    return xb.to(device), yb.to(device)
+# dbg_input_output()
+
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off logits for the next token from the lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        # Pluck out a row from the embedding table for every item in the input
+        logits = self.token_embedding_table(idx) # B(atch), T(ime), C(hannel)
+        if targets is None: 
+            loss = None
+        else:
+            # pytorch expects B,C,T
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C) # Stretch the inputs out so the second dim is C
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens): 
+        # idx -> (B,T) (current context)
+        # At this point B is always 1 because we only forward one batch
+        for _ in range(max_new_tokens):
+          # get the predictions
+          logits, _ = self(idx) # (B, T, C)
+          # we get logits for all items in the current block (T) but
+          # we only focus only on the last time step since we need the next char
+          logits = logits[:, -1, :] # becomes (B, C)
+          # apply softmax to get probabilities
+          probs = F.softmax(logits, dim=-1) # (B, C)
+          # sample from the distribution
+          idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+          # append sampled index to the running sequence
+          idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+
+model = BigramLanguageModel(vocab_size).to(device)
+
+# gets loss for multiple batches of each of eval and train splits
+@torch.no_grad()
+def estimate_loss():
+  out = {}
+  model.eval()
+  for split in ['train', 'val']:
+      losses = torch.zeros(eval_iters)
+      for k in range(eval_iters):
+          X, Y = get_batch(split)
+          logits, loss = model(X, Y)
+          losses[k] = loss.item()
+      out[split] = losses.mean()
+  model.train()
+  return out
+
+
+def dbg_generate_dims(): # use with smaller block_size to understand what's going on in generate
+    xb, yb = dbg_input_output()
+
+    logits, loss = model(xb, yb)
+
+    print(logits.shape)
+    print(loss)
+
+    idx = torch.zeros((1,1), dtype = torch.long)
+    print(decode(model.generate(idx, max_new_tokens=100)[0].tolist()))
+# dbg_generate_dims()
+
+'''
+Mathematical trick for efficient self-attention impl
+
+we have 8 tokens in a batch that don't talk to each other but we need to couple them
+we need to have the token at ith location talk to the prev tokens (i-1, i-2, ...)
+simplest way is to average the prev tokens (incl token i)
+    becomes feature vec that summarizes token i in context 
+    quite lossy, eg loses spatial arrangement - will deal with this later
+'''
+def dbg_self_attn_math_trick():
+    torch.manual_seed(11)
+    B, T, C = 4, 8, 2
+    x = torch.randn(B,T,C)
+    print(x.shape)
+
+    # inefficient way::
+    # We want x[b, t] = mean_{i<=t} x[b, i]
+    xbow = torch.zeros((B, T, C)) # bow = bag of words
+    for b in range(B):
+      for t in range(T):
+          xprev = x[b, :t+1]  # (t, C)
+          xbow[b, t] = torch.mean(xprev, 0)
+
+    print(x[0][0]);print(xbow[0][0]) # these should be equal bc we avg only one token
+    print('--')
+    print(x[0][1]);print(xbow[0][1]) # NOT be equal, xbow is avg of first and second token
+
+    # more efficient way ::
+
+    def dbg_tril_mean():
+        # tril workings + mat mul
+        a = torch.tril(torch.ones(3,3)) # lower triangular ones
+        b = torch.randint(0, 10, (3,2)).float()
+        c = a @ b
+        print(a)
+        print(b)
+        # each row of c has the running sum for each batch
+        print(c)
+        print(torch.equal(c[0], b[0]))
+        print(torch.equal(c[1], b[:2].sum(dim=0)))
+        print(torch.equal(c[2], b[:3].sum(dim=0)))
+
+        # prev was sum, this is mean
+        a = torch.tril(torch.ones(3,3)) # lower triangular ones
+        a = a / torch.sum(a, 1, keepdim=True) # <---- mean here bc the ones are now mean
+        b = torch.randint(0, 10, (3,2)).float()
+        c = a @ b
+        print(a)
+        print(b)
+        print(c)
+        print(torch.allclose(c[0], b[0]))
+        print(torch.allclose(c[1], b[:2].mean(dim=0)))
+        print(torch.allclose(c[2], b[:3].mean(dim=0)))
+    # dbg_tril_mean()
+
+    # now with the B, T, C example above
+    # i.e. do the above for the entire batch
+    wei = torch.tril(torch.ones(T, T))
+    wei = wei / wei.sum(1, keepdim=True)
+    print(wei)
+
+    # (T,T) @ (B, T , C) --> (B,T,T) @ (B,T,C)  -- torch adds the dim
+    # (B,T,T) @ (B,T,C) -- multiplies for each batch --> (B,T,C)
+    xbow2 = wei @ x
+    print(torch.allclose(xbow, xbow2))
+
+    # V3 xbow:
+    # uses torch.softmax
+    # We will use this version bc weights start at zeros
+    # this is a representation of "interaction strength" / "affinity"
+    # we can train on the wei matrix too (I think)
+    tril = torch.tril(torch.ones(T, T))
+    wei = torch.zeros((T,T))
+    wei = wei.masked_fill(tril == 0, float('-inf'))
+    wei = F.softmax(wei, dim = 1) # same as prev wei
+    xbow3 = wei @ x
+    print(torch.allclose(xbow2, xbow3))
+# dbg_self_attn_math_trick()
+
+
+# In make more we use stochastic descent but Adam optimizer works better
+# We can set lr to 1e-3 (quite high) bc our model is small
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+for iter in range(max_iters):
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: {losses}")
+
+    xb, yb = get_batch('train')
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+ctx = torch.zeros((1,1), dtype = torch.long).to(device)
+print(decode(model.generate(ctx, max_new_tokens=1000)[0].tolist()))
+
