f8f56d2daf3cb92cc0364ff3d4e16fc2d8e52f7e
Sriram Sundarraj <ssundarraj@gmail.com>
2026-08-03 19:58:01 -0700

    gpt2: dataloader

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -1,6 +1,7 @@
 # Repo: https://github.com/karpathy/build-nanogpt
 
 import sys
+import tiktoken
 import math
 from dataclasses import dataclass
 import torch
@@ -164,11 +165,43 @@ class GPT(nn.Module):
 
         return model
 
+class DataLoaderLite:
+  def __init__(self, B, T):
+      self.B = B
+      self.T = T
+
+      # at init load tokens from disk and store them in memory
+      with open('input.txt', 'r') as f:
+          text = f.read()
+      enc = tiktoken.get_encoding('gpt2')
+      tokens = enc.encode(text)
+      self.tokens = torch.tensor(tokens)
+      print(f"loaded {len(self.tokens)} tokens")
+      print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+      # state
+      self.current_position = 0
+
+  def next_batch(self):
+      B, T = self.B, self.T
+      buf = self.tokens[self.current_position : self.current_position+B*T+1]
+      x = (buf[:-1]).view(B, T) # inputs
+      y = (buf[1:]).view(B, T) # targets
+      # advance the position in the tensor
+      self.current_position += B * T
+      # if loading the next batch would be out of bounds, reset
+      if self.current_position + (B * T + 1) > len(self.tokens):
+          self.current_position = 0
+      return x, y
+
+
 device = (
     "cuda" if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available()
     else "cpu"
 )
+# torch.manual_seed(1337)
+
 
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
@@ -176,29 +209,13 @@ print("using device: ", device)
 model.train()
 model.to(device)
 
-num_return_sequences = 5
-max_length = 30
-
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello what's up")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to(device)
-
-with open('input.txt', 'r') as f:
-    text = f.read()
-
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T+1]).to(device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+train_loader = DataLoaderLite(B=4, T=32)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
@@ -207,9 +224,13 @@ for i in range(50):
 sys.exit(0)
 
 
+
 model.eval()
 
 # generate! right now x is (B, T) where B = 5, T = 8
+num_return_sequences = 5
+max_length = 30
+
 # set the seed to 42
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
@@ -237,4 +258,3 @@ for i in range(num_return_sequences):
     tokens = x[i, :max_length].tolist()
     decoded = enc.decode(tokens)
     print(">", decoded)
-
