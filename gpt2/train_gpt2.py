6a99ae03e774bc4e168521a400c16c92660d1a05
Sriram Sundarraj <ssundarraj@gmail.com>
2026-08-03 20:33:09 -0700

    gpt: model init

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -40,6 +40,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1 
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -72,7 +73,7 @@ class Block(nn.Module):
 
     def __init__(self, config: GPTConfig):
         super().__init__()
-        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.ln_1 = nn.LayerNorm(config.n_embd) 
         self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         # feed forward
@@ -99,6 +100,21 @@ class GPT(nn.Module):
 
       # These share weights
       self.transformer.wte.weight = self.lm_head.weight # type: ignore[operator]
+
+      self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        # torch default init is good for LayerNorm
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 
 
     def forward(self, idx, targets = None):
