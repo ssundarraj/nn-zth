b38a98c1396da508926527afbab33bced09990b2
Sriram Sundarraj <ssundarraj@gmail.com>
2026-08-03 18:25:16 -0700

    gpt2: run gpt2 pretrained weights

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -1,3 +1,5 @@
+# Repo: https://github.com/karpathy/build-nanogpt
+
 import math
 from dataclasses import dataclass
 import torch
@@ -6,11 +8,11 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
 
 class MLP(nn.Module):
 
@@ -77,6 +79,7 @@ class Block(nn.Module):
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
+        return x
 
 class GPT(nn.Module):
 
@@ -92,4 +95,111 @@ class GPT(nn.Module):
       ))
       self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+    def forward(self, idx):
+        B, T = idx.shape
+        assert T < self.config.block_size
+        pos = torch.arange(0, T, dtype=torch.long, device = idx.device)
+        pos_emb = self.transformer.wpe(pos) # type: ignore[operator]
+        tok_emb = self.transformer.wte(idx) # type: ignore[operator]
+        x = tok_emb + pos_emb
+        for block in self.transformer.h: # type: ignore[operator]
+            x = block(x)
+        x = self.transformer.ln_f(x) # type: ignore[operator]
+        logits = self.lm_head(x)
+        return logits
+
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+model = GPT.from_pretrained('gpt2')
+print("weights loaded")
+model.eval()
+model.to('mps')
+
+num_return_sequences = 5
+max_length = 30
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello what's up")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to('mps')
+
+
+# generate! right now x is (B, T) where B = 5, T = 8
+# set the seed to 42
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+while x.size(1) < max_length:
+  # forward the model to get the logits
+  with torch.no_grad():
+      logits = model(x) # (B, T, vocab_size)
+      # take the logits at the last position
+      logits = logits[:, -1, :] # (B, vocab_size)
+      # get the probabilities
+      probs = F.softmax(logits, dim=-1)
+      # do top-k sampling of 50 (huggingface pipeline default)
+      # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+      topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+      # select a token from the top-k probabilities
+      ix = torch.multinomial(topk_probs, 1) # (B, 1)
+      # gather the corresponding indices
+      xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+      # append to the sequence
+      x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
 
